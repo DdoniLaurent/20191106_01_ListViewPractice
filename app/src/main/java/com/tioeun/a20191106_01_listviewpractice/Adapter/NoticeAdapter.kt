80cde81d1181ceaca84fbc8a67127f3bdd941ad9
YongMinSun <ddoni1010@gmail.com>
2019-11-06 20:38:08 +0900

    어댑터 클래스 구조 생성

---
+++ app/src/main/java/com/tioeun/a20191106_01_listviewpractice/Adapter/NoticeAdapter.kt
@@ -0,0 +1,29 @@
+package com.tioeun.a20191106_01_listviewpractice.Adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.tioeun.a20191106_01_listviewpractice.R
+import com.tioeun.a20191106_01_listviewpractice.datas.NoticeData
+
+class NoticeAdapter(context: Context, resource:Int, list:ArrayList<NoticeData>) : ArrayAdapter<NoticeData>(context, R.layout.notice_list_item, list){
+
+    var mCOntext = context
+    var mList = list
+    var inf = LayoutInflater.from(context)
+
+    constructor(context: Context, list: ArrayList<NoticeData>) : this (context, R.layout.notice_list_item, list)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+
+        if(tempRow == null){
+            tempRow = inf.inflate(R.layout.notice_list_item, null)
+        }
+//        tempRow가 null 인 경우를 대비했으니, row에 tempRow는 절대 null이 아니라고 하면서 대입
+        var row = tempRow!!
+        return row
+    }
+}
--- app/src/main/java/com/tioeun/a20191106_01_listviewpractice/MainActivity.kt
+++ app/src/main/java/com/tioeun/a20191106_01_listviewpractice/MainActivity.kt
@@ -2,11 +2,27 @@ package com.tioeun.a20191106_01_listviewpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tioeun.a20191106_01_listviewpractice.datas.NoticeData
 
 class MainActivity : AppCompatActivity() {
+
+    var noticeList = ArrayList<NoticeData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun addNoties(){
+        noticeList.add(NoticeData("1번 공지", "1번 내용입니다.", "2019-01-01"))
+        noticeList.add(NoticeData("2번 공지", "2번 내용입니다.", "2019-02-05"))
+        noticeList.add(NoticeData("3번 공지", "3번 내용입니다.", "2019-03-07"))
+        noticeList.add(NoticeData("4번 공지", "4번 내용입니다.", "2019-04-15"))
+        noticeList.add(NoticeData("5번 공지", "5번 내용입니다.", "2019-05-05"))
+        noticeList.add(NoticeData("6번 공지", "6번 내용입니다.", "2019-06-13"))
+        noticeList.add(NoticeData("7번 공지", "7번 내용입니다.", "2019-07-31"))
+        noticeList.add(NoticeData("8번 공지", "8번 내용입니다.", "2019-8-16"))
+        noticeList.add(NoticeData("9번 공지", "9번 내용입니다.", "2019-09-26"))
+        noticeList.add(NoticeData("10번 공지", "10번 내용입니다.", "2019-01-01"))
     }
 }
