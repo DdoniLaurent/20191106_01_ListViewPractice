26d23d3787b0ac9bf2992f64019d5933d170793b
YongMinSun <ddoni1010@gmail.com>
2019-11-06 19:56:16 +0900

    데이터 클래스 생성

---
+++ app/src/main/java/com/tioeun/a20191106_01_listviewpractice/datas/NoticeData.kt
@@ -0,0 +1,8 @@
+package com.tioeun.a20191106_01_listviewpractice.datas
+
+class NoticeData(intputTitle:String, content:String, writeDate:String) {
+
+    var title = intputTitle
+    var content = content
+    var date = writeDate
+}
